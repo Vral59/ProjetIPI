256c3c8733894d23756b7ebcccda9056331508f2
Vral59 <57286373+Vral59@users.noreply.github.com>
2021-11-20 23:55:55 +0100

    Add files via upload

--- projetV1.c
+++ projetV1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pile.h"
+#include "listech.h"
+
 
 /*
 @requires One arguements, an automata .aut
@@ -8,21 +10,65 @@
 @ensures recognize langage 
 
 https://www.tutorialspoint.com/cprogramming/c_file_io.html
+https://pages.lip6.fr/Jean-Francois.Perrot/inalco/cours06/Cours6/TransC/LirOct.html
 */
-int main(int argc,char *argv[]){     
-   FILE *fp;
-   char buff[255];
 
-   fp = fopen(argv[1], "r");
-   fscanf(fp, "%s", buff);
-   printf("1 : %s\n", buff );
+#define TAILLE_MAX 1000
 
-   fgets(buff, 255, (FILE*)fp);
-   printf("2: %s\n", buff );
-   
-   fgets(buff, 255, (FILE*)fp);
-   printf("3: %s\n", buff );
-   fclose(fp);
+int main(int argc, char *argv[]) {
+	FILE* fentr = NULL;
 
-   return 0;
+   /* Test d'entrée */
+
+	if (argc!=2) {
+    		printf("Il me faut un fichier");
+    		exit(1);
+	}
+	if ((fentr= fopen(argv[1], "r")) == NULL) {
+     		printf("Je ne peux pas ouvrir %s", argv[1]);
+     		exit(1);
+	}
+
+   /* Déclaration des variables */
+
+	char chaine[TAILLE_MAX] = "";
+   fgets(chaine, TAILLE_MAX, fentr); /* On récupère la première ligne */
+   int n = chaine[2] - '0'; /* le caractère après le a est transformé en int */
+   int fin = '\225';
+   int val;
+   int action[n*128];
+   int reduit1[n];
+   int reduit2[n];
+   int decale[n*128];
+   int branchement[n*128];
+
+   /* Remplissage des tableaux contenant les actions de l'automate */
+
+   for (int i = 0; i < (n-1)*128; i++){
+      action[i] = fgetc(fentr);
+      printf("%3d",action[i]);
+   }
+   printf("\n\n");
+
+   for (int i = 0; i < n; i++){
+      reduit1[i] = fgetc(fentr);
+      printf("%3d",reduit1[i]);
+   } 
+   printf("\n\n");
+
+   for (int i = 0; i < n; i++){
+      reduit2[i] = fgetc(fentr);
+      printf("%3d",reduit2[i]);
+   }
+   printf("\n");
+
+   int i = 0;
+   while((val = fgetc(fentr)) != fin){
+      decale[i] = val;
+      printf("%2d",val);
+      i++;
+   }
+
+	fclose(fentr);
+	return 0;
 }
