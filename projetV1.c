de88593437f1cc0f6f329789216399f6d58196b9
Vral59 <valentin@buchon.fr>
2021-11-23 19:04:29 +0100

    Correction bug + malloc

--- projetV1.c
+++ projetV1.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pile.h"
-#include "listech.h"
 
 
 /*
 @requires One arguements, an automata .aut
 @assings ??
 @ensures recognize langage 
-
-https://www.tutorialspoint.com/cprogramming/c_file_io.html
-https://pages.lip6.fr/Jean-Francois.Perrot/inalco/cours06/Cours6/TransC/LirOct.html
 */
 
-#define TAILLE_MAX 1000
+
 
 int main(int argc, char *argv[]) {
 	FILE* fentr = NULL;
@@ -29,47 +25,88 @@ int main(int argc, char *argv[]) {
      	exit(1);
 	}
 
-   /* Déclaration des variables */
 
-	char chaine[TAILLE_MAX] = "";
-   fgets(chaine, TAILLE_MAX, fentr); /* On récupère la première ligne avec le 'a n' */
-   int n = chaine[2] - '0'; /* le caractère n et on le transforme en int avec de la magie*/
+   /* Récupération de n */
+
+	fgetc(fentr); /* On passe le 'a' */
+   fgetc(fentr); /* On passe le ' ' */
+   int n = 0;
    char val;
-   int action[n*128];
-   int reduit1[n];
-   int reduit2[n];
-   int decale[n*128]; /* Ici on a aucune idée de la taille qu'il faut, je ferai des liste chainé plus tard, ici on prend large */
-   int branchement[n*128]; /* Idem */
+   while ((val = fgetc(fentr)) != '\n'){ /* n peut s'écrire sur plusieurs caractères */
+      n = n*10 + val - '0'; /* le caractère n et on le transforme en int */
+   }
+   printf("Voici n : %i \n",n);
+   
+
+
+   /* déclaration des différentes valeurs contenu dans le fichier */
+   int *action = NULL;
+   int *reduit1 = NULL;
+   int *reduit2 = NULL;
+   int *decale = NULL;
+   int *branchement = NULL;
+
+   action = malloc((n*128) * sizeof(int));
+   reduit1 = malloc((n*sizeof(int)));
+   reduit2 = malloc((n*sizeof(int)));
+   decale = (int *)malloc((n*128) * sizeof(int));
+   branchement = (int *)malloc((n*128) * sizeof(int));
+
+
 
    /* Remplissage des tableaux contenant les actions de l'automate */
 
    for (int i = 0; i < (n-1)*128; i++){
       action[i] = fgetc(fentr);
-      printf("%3d",action[i]);
+      printf("%3d",action[i]); // Les prochains printf seront la pour afficher les valeurs et tout vérifier
    }
    printf("\n\n");
-
+   fgetc(fentr); /* on vire le '\n' */
    for (int i = 0; i < n; i++){
       reduit1[i] = fgetc(fentr);
       printf("%3d",reduit1[i]);
    } 
    printf("\n\n");
-
+   fgetc(fentr); /* on vire le '\n' */
    for (int i = 0; i < n; i++){
       reduit2[i] = fgetc(fentr);
       printf("%3d",reduit2[i]);
    }
    printf("\n");
+   fgetc(fentr); /* on vire le '\n' */
 
-	/* Ici ça ne fonctionne plus, on cherche les '\225' */
-   int i = 0;
-   while((val = fgetc(fentr)) != '\225'){
-      int tmp = val;
-      decale[i] = tmp;
-      printf("voici le char : %2c, voici le int : %2i \n",val,tmp);
-      i++;
+   /* Récupération de decale*/
+   char value1;
+   while ((value1 = fgetc(fentr)) != '\255'){
+      int etat1 = (int)value1; // Cast explicite
+      int *lettre = fgetc(fentr); //Le numéro du caractère va donner sa place dans le tableau 2D
+      int etat2 = fgetc(fentr);
+      printf("%i %i %i \n",etat1,lettre,etat2);
+      decale[etat1][lettre] = etat2;
+   }
+   
+   printf("\n\n");
+   fgetc(fentr); // On vire les deux '\255' qu'il reste
+   fgetc(fentr);
+   /* récupération de branchement */
+   char value2;
+   while ((value2 = fgetc(fentr)) != '\255'){
+      int etat1 = (int)value2; // Cast explicite
+      int *lettre = fgetc(fentr);
+      int etat2 = fgetc(fentr);
+      printf("%i %i %i \n",etat1,lettre,etat2);
+      branchement[etat1][lettre] = etat2;
    }
 
+   /* On a finit de lire tout le fichier */
+
+
+   /* Libération de la mémoire */
 	fclose(fentr);
-	return 0;
+   free(action);
+   free(reduit1);
+   free(reduit2);
+   free(decale);
+   free(branchement);
+	return 0; 
 }
